4f2f8f18d123394a24bce23c224213fb210b548b
Tammy Metz <tammy@tammymetz.com>
2019-01-20 18:56:08 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,17 +3,23 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def new
+  end
+
   def edit
     @post = Post.find(params[:id])
   end
 
+  def create
+  end
+
   def update
     @post = Post.find(params[:id])
-
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
-  end
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
 
   private
 
