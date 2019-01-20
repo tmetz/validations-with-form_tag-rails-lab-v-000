216b4d581e4afcf67354a65ac5beda1e5f8d4b67
Tammy Metz <tammy@tammymetz.com>
2019-01-20 18:57:06 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -18,15 +18,16 @@ class AuthorsController < ApplicationController
   end
 
   def edit
-  end
-
-  def create
-    @author = Author.create(author_params)
-
-    redirect_to author_path(@author)
+    @author = Author.find(params[:id])
   end
 
   def update
+    @author = Author.find(params[:id])
+    if @author.update(author_params)
+      redirect_to author_path(@author)
+    else
+      render :edit
+    end
   end
 
   private
