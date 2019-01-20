4af21c43f3805028742f1b033c4254abb865f455
Tammy Metz <tammy@tammymetz.com>
2019-01-20 18:54:02 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -8,9 +8,25 @@ class AuthorsController < ApplicationController
   end
 
   def create
+    @author = Author.new(author_params)
+    if @author.valid?
+      @author.save
+      redirect_to author_path(@author)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def create
     @author = Author.create(author_params)
 
     redirect_to author_path(@author)
+  end
+
+  def update
   end
 
   private
